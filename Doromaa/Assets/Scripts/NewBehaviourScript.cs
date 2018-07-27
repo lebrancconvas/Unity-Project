dbd9cca36aa36f9101b092d3836deaac558a90af
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-07-27 23:32:28 +0700

    Initial Commit

---
+++ Doromaa/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    private Rigidbody rd;
+    public float speed;
+    public float attack;
+	// Use this for initialization
+	void Start () {
+        rd = GetComponent<Rigidbody>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+        rd.AddForce(movement*speed*attack);
+	}
+}
